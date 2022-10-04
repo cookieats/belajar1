41332fa25db8e69ced7be6cce7e0a4c3e0aeaee1
cookieats <75973839+cookieats@users.noreply.github.com>
2022-10-04 09:21:58 +0800

    main pages

---
+++ belajar1/lib/login.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class login extends StatelessWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 2, 14, 147),
+             title: Center(
+              child: const Text("Koperasi Undiksha"),
+            ),
+            // actions: [
+            //   Container(
+            //     child: Icon(Icons.logout),
+            //     padding: EdgeInsets.all(10),
+            //   )
+            // ],
+          ),
+      body: Column(
+            children: [
+              Container(
+                  padding: EdgeInsets.all(25),
+                  child: Center(
+                    child: Image.asset(
+                      'Assets/img/logo1.jpg',
+                      width: 200,
+                      height: 200,
+                      fit: BoxFit.contain,
+                    ),
+                  )),
+              Container(
+                child: Column(children: [
+                  Container(
+                    alignment: Alignment.topLeft,
+                    margin: EdgeInsets.fromLTRB(30, 0, 0, 5),
+                    child: Text('Username'),
+                  ),
+                  SizedBox(
+                    width: 350,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(20),
+                        border: OutlineInputBorder(),
+                        hintText: 'masukan username anda',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    margin: EdgeInsets.fromLTRB(30, 10, 0, 5),
+                    child: Text('Password'),
+                  ),
+                  SizedBox(
+                    width: 350,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(20),
+                        border: OutlineInputBorder(),
+                        hintText: 'masukan password anda',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 15, 0, 30),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        fixedSize: Size(200, 35),
+                      ),
+                      onPressed: () {},
+                      child: Text('login'),
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        TextButton(
+                            onPressed: () {}, child: Text('Datar Mbanking')),
+                        TextButton(
+                            onPressed: () {}, child: Text('lupa password?')),
+                      ],
+                    ),
+                  )
+                ]),
+              ),
+            SizedBox(
+              height: 280,
+            ),
+             Container(
+              height: 47,
+              width: 500,
+              color: Colors.grey,
+              alignment: Alignment.center,
+              child: Text('copyright @2022 by undiksha'),
+             )
+            ],
+          )
+    );
+  }
+}
--- belajar1/lib/main.dart
+++ belajar1/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:belajar1/login.dart';
+import 'package:belajar1/utama.dart';
 import 'package:flutter/material.dart';
 import 'package:footer/footer.dart';
 import 'package:footer/footer_view.dart';
@@ -13,99 +15,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color.fromARGB(255, 2, 14, 147),
-            title: Center(
-              child: const Text("Koperasi Undiksha"),
-            ),
-            actions: [
-              Container(
-                child: Icon(Icons.logout),
-                padding: EdgeInsets.all(10),
-              )
-            ],
-          ),
-          body: Column(
-            children: [
-              Container(
-                  padding: EdgeInsets.all(25),
-                  child: Center(
-                    child: Image.asset(
-                      'Assets/img/logo1.jpg',
-                      width: 200,
-                      height: 200,
-                      fit: BoxFit.contain,
-                    ),
-                  )),
-              Container(
-                child: Column(children: [
-                  Container(
-                    alignment: Alignment.topLeft,
-                    margin: EdgeInsets.fromLTRB(30, 0, 0, 5),
-                    child: Text('Username'),
-                  ),
-                  SizedBox(
-                    width: 350,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(20),
-                        border: OutlineInputBorder(),
-                        hintText: 'masukan username anda',
-                      ),
-                    ),
-                  ),
-                  Container(
-                    alignment: Alignment.topLeft,
-                    margin: EdgeInsets.fromLTRB(30, 10, 0, 5),
-                    child: Text('Password'),
-                  ),
-                  SizedBox(
-                    width: 350,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(20),
-                        border: OutlineInputBorder(),
-                        hintText: 'masukan password anda',
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(0, 15, 0, 30),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.blue,
-                        fixedSize: Size(200, 35),
-                      ),
-                      onPressed: () {},
-                      child: Text('login'),
-                    ),
-                  ),
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        TextButton(
-                            onPressed: () {}, child: Text('Datar Mbanking')),
-                        TextButton(
-                            onPressed: () {}, child: Text('lupa password?')),
-                      ],
-                    ),
-                  )
-                ]),
-              ),
-            SizedBox(
-              height: 280,
-            ),
-             Container(
-              height: 47,
-              width: 500,
-              color: Colors.grey,
-              alignment: Alignment.center,
-              child: Text('copyright @2022 by undiksha'),
-             )
-            ],
-          )),
+       title: 'DRAP',
+      theme: ThemeData.light(),
+      home: Utama(),
     );
   }
 }
+
+
